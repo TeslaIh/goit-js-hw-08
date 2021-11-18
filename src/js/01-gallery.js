691275d336552aa312eb0cd033c3c554782d24cc
TeslaIh <87144117+TeslaIh@users.noreply.github.com>
2021-11-18 17:00:24 +0200

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
-// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
+// Add imports above this line
+
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryList = createGalleryImagesList(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryList);
+
+function createGalleryImagesList(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+  <li class = "gallery__item">
+  <a class = "gallery__link" href="${original}">
+  <img
+  class="gallery__image"
+  src="${preview}"
+  data-source="${original}"
+  alt="${description}"
+  title="${description}"
+  />
+  </a>
+  </li>
+  `;
+    }).join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData:"alt",
+    captionPosition:'bottom',
+    captionDelay: 250
+});
